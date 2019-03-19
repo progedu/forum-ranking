968df7415d1575207f1cf2925d317e16b2af92d2
DDRAGON <davide.0829@gmail.com>
2019-03-19 19:36:59 +0900

    test でクローリングするよう実装

--- index.js
+++ index.js
@@ -1,79 +1,65 @@
-$.ajax({
-    url: 'answerUsersnainai.json',
-    statusCode: {
-        404: function() {
-            console.log("page not found");
+function calc(allSpanUserJson, monthlyAnswersNewJson, monthlyAnswersOldJson) {
+    // 全期間
+    const ranking50 = allSpanUserJson.slice(0, 50);
+    // 今月
+    const monthlyRanking50 = monthlyAnswersNewJson.slice(0, 50);
+    const monthlyRanking50_old = monthlyAnswersOldJson.slice(0, 50);
+
+    // 変更を反映させる。
+    for (let [nowRank, nowValue] of monthlyRanking50.entries()) {
+        monthlyRanking50[nowRank].change = `↑`; // 過去にはいないということは上昇！
+        for (let [oldRank, oldValue] of monthlyRanking50_old.entries()) {
+            if (nowValue.userId === oldValue.userId) { // 発見！
+                if (nowRank > oldRank) {
+                    monthlyRanking50[nowRank].change = `↓`;
+                } else if (nowRank === oldRank) { // 不変;
+                    monthlyRanking50[nowRank].change = `-`;
+                }
+                break;
+            }
         }
     }
-});
+
+    return [ranking50, monthlyRanking50];
+}
+
+function display(ranking50, monthlyRanking50) {
+    for (let [index, userObj] of monthlyRanking50.entries()) {
+        const liDom = $('<li>', {
+            class: 'list-group-item'
+        });
+        const div1Dom = $('<div>', {
+            text: '' + (index + 1) + '位　' + userObj.userName + ' さん　' + userObj.change
+        });
+        const div2Dom = $('<div>', {
+            text: '回答件数: ' + userObj.answeredQuestionMany + ' 総合回数: ' + userObj.total
+        });
+        liDom.append(div1Dom);
+        liDom.append(div2Dom);
+        $('ul#monthly-ranking').append(liDom);
+    }
+
+    for (let [index, userObj] of ranking50.entries()) {
+        const liDom = $('<li>', {
+            class: 'list-group-item'
+        });
+        const div1Dom = $('<div>', {
+            text: '' + (index + 1) + '位　' + userObj.userName + ' さん'
+        });
+        const div2Dom = $('<div>', {
+            text: '回答件数: ' + userObj.answeredQuestionMany + ' 総合回数: ' + userObj.total
+        });
+        liDom.append(div1Dom);
+        liDom.append(div2Dom);
+        $('ul#all-ranking').append(liDom);
+    }
+}
 
 $.getJSON('answerUsers.json', function(allSpanUserJson) {
-    $.getJSON('monthlyAnswers_2019_03_18.json', function(json) {
-        console.log(json);
+    $.getJSON('monthlyAnswers_new.json', function(monthlyAnswersNewJson) {
+        $.getJSON('monthlyAnswers_old.json', function(monthlyAnswersOldJson) {
+            const [ranking50, monthlyRanking50] = calc(allSpanUserJson, monthlyAnswersNewJson, monthlyAnswersOldJson);
+            display(ranking50, monthlyRanking50);
+        });
     });
 });
-
-
-function sortAnswersData(answersData) {
-    return answersData.sort(function(a, b) {
-        if (a.answeredQuestionMany > b.answeredQuestionMany) {
-            return -1;
-        } else if (a.answeredQuestionMany < b.answeredQuestionMany) {
-            return 1;
-        } else {
-            if (a.total > b.total) {
-                return -1;
-            } else if (a.total < b.total) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    });
-}
-
-function calc() {
-    try {
-        // 全期間
-        const allSpanUserJson = JSON.parse(fs.readFileSync('answerUsers.json', 'utf8'));
-        ranking50 = sortAnswersData(allSpanUserJson).slice(0, 50);
-
-        // 今月
-        const thisYearMonthDay = moment().tz("Asia/Tokyo").format('YYYY_MM_DD');
-        const thisTimeStanp = moment().tz("Asia/Tokyo");
-        const yesterDayYearMonthDay = moment(thisTimeStanp - 1000 * 60 * 60 * 24).tz("Asia/Tokyo").format('YYYY_MM_DD');
-        const datBeforeYesterdayYearMonthDay = moment(thisTimeStanp - 1000 * 60 * 60 * 24 * 2).tz("Asia/Tokyo").format('YYYY_MM_DD');
-
-        // おとといのデータがあれば削除
-        try { fs.unlinkSync(`monthlyAnswers_${datBeforeYesterdayYearMonthDay}.json`); } catch (e) {}
-        // ランキング作成
-        const thisMonthUserJson = JSON.parse(fs.readFileSync(`monthlyAnswers_${thisYearMonthDay}.json`, 'utf8'));
-        monthlyRanking50 = sortAnswersData(thisMonthUserJson).slice(0, 50);
-
-        // 変更を反映させる。
-        try {
-            const thisMonthUserJson_old = JSON.parse(fs.readFileSync(`monthlyAnswers_${yesterDayYearMonthDay}.json`, 'utf8'));
-            const monthlyRanking50_old = sortAnswersData(thisMonthUserJson_old).slice(0, 50);
-
-            for (let [nowRank, nowValue] of monthlyRanking50.entries()) {
-                monthlyRanking50[nowRank].change = `↑`; // 過去にはいないということは上昇！
-                for (let [oldRank, oldValue] of monthlyRanking50_old.entries()) {
-                    if (nowValue.userId === oldValue.userId) { // 発見！
-                        if (nowRank < oldRank) {
-                            monthlyRanking50[nowRank].change = `↓`;
-                        } else if (nowRank === oldRank) { // 不変;
-                            monthlyRanking50[nowRank].change = `-`;
-                        }
-                        break;
-                    }
-                }
-            }
-        } catch (e) {}
-
-    } catch (e) {
-        console.log(e);
-        return [];
-    }
-}
-
-calc();
---
+++ test.js
@@ -0,0 +1,141 @@
+const puppeteer = require('puppeteer');
+const moment = require('moment-timezone');
+const fs = require('fs');
+
+const LIMIT_MAX = 50;
+const answerUsersMap = new Map();
+console.log('today is ' + moment().tz("Asia/Tokyo").format('YYYY-MM-DD HH:mm'));
+const thisYear = moment().tz("Asia/Tokyo").format('YYYY');
+const thisMonth = moment().tz("Asia/Tokyo").format('MM');
+let writeToMonthFileFlag = true;
+
+function fetchForumAPI(offset, limit) {
+    console.log(`offset = ${offset}, limit = ${limit}`);
+    return new Promise(function(resolve, reject) {
+        (async() => {
+            try {
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                const response = await page.goto(`https://api.nnn.ed.nico/v1/questions?offset=${offset}&limit=${limit}`);
+
+                const contentsJson = await page.evaluate(() => {
+                    return JSON.parse(document.querySelector("body pre").innerText);
+                });
+
+                if (contentsJson.questions.length === 0) {
+                    await browser.close();
+                    resolve('finish!');
+                    return;
+                }
+
+                for (let contest of contentsJson.questions) {
+                    const questionUserId = contest.user.id;
+                    const questionUserName = contest.user.name;
+                    const questionTimeStamp = contest.created_at;
+                    const questionTimeYear = moment(questionTimeStamp * 1000).tz("Asia/Tokyo").format('YYYY');
+                    const questionTimeMonth = moment(questionTimeStamp * 1000).tz("Asia/Tokyo").format('MM');
+                    if (writeToMonthFileFlag === true && (questionTimeYear !== thisYear || questionTimeMonth !== thisMonth)) {
+                        writeToMonthFileFlag = false;
+                        console.log('copy file new to old.');
+                        fs.copyFileSync('monthlyAnswers_new.json', 'monthlyAnswers_old.json');
+                        console.log('write monthly file to new.');
+                        writeToJsonFile(answerUsersMap, 'monthlyAnswers_new.json');
+                    }
+
+                    const answersManyMap = new Map();
+
+
+                    for (let answer of contest.answers) {
+                        const answerUserId = answer.user.id;
+                        const answerUserName = answer.user.name;
+                        if (questionUserId === answerUserId) {
+                            continue;
+                        }
+
+                        if (!answersManyMap.has(answerUserId)) { // 新規
+                            answersManyMap.set(answerUserId, { answerUserName, answerMany: 1 });
+                        } else { // 重複
+                            const answerManyObj = answersManyMap.get(answerUserId);
+                            answerManyObj.answerMany += 1;
+                            answersManyMap.set(answerUserId, answerManyObj);
+                        }
+                    }
+
+                    for (let [answerUserId, answerManyObj] of answersManyMap) {
+                        if (!answerUsersMap.has(answerUserId)) { // 新規
+                            answerUsersMap.set(answerUserId, { userName: answerManyObj.answerUserName, total: answerManyObj.answerMany, answeredQuestionMany: 1 });
+                        } else { // 重複
+                            const answerUser = answerUsersMap.get(answerUserId);
+                            answerUser.total += answerManyObj.answerMany;
+                            answerUser.answeredQuestionMany += 1;
+                            answerUsersMap.set(answerUserId, answerUser);
+                        }
+                    }
+                }
+
+                await browser.close();
+                return resolve('sucsess and contiune');
+            } catch (e) {
+                return reject(e);
+            }
+        })();
+    });
+}
+
+function sortAnswersData(answersData) {
+    return answersData.sort(function(a, b) {
+        if (a.answeredQuestionMany > b.answeredQuestionMany) {
+            return -1;
+        } else if (a.answeredQuestionMany < b.answeredQuestionMany) {
+            return 1;
+        } else {
+            if (a.total > b.total) {
+                return -1;
+            } else if (a.total < b.total) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    });
+}
+
+function writeToJsonFile(answerUsersMap, fileName) {
+    let outPutJSON = [];
+    for ([userId, answerObj] of answerUsersMap) {
+        const pushObj = {
+            userId,
+            userName: answerObj.userName,
+            total: answerObj.total,
+            answeredQuestionMany: answerObj.answeredQuestionMany
+        };
+        outPutJSON.push(pushObj);
+    }
+
+    outPutJSON = sortAnswersData(outPutJSON);
+
+    fs.writeFileSync(fileName, JSON.stringify(outPutJSON, null, '   '));
+}
+
+function fetchConroler(offset) {
+
+    fetchForumAPI(offset, LIMIT_MAX).then((nonQuestionAnswers) => {
+        if (nonQuestionAnswers === 'finish!') {
+            console.log('crowring finish');
+            writeToJsonFile(answerUsersMap, 'answerUsers.json');
+            return;
+        }
+
+        // sucsess
+        const nestOffset = offset + LIMIT_MAX;
+        setTimeout(fetchConroler, 3000, nestOffset);
+
+    }).catch((e) => {
+        console.log(e);
+        console.log('retry anyway!');
+        setTimeout(fetchConroler, 1000 * 60, offset);
+        return;
+    });
+}
+
+fetchConroler(0);
